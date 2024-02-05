383fdd120f8c1515fa7410866a2db0bf9991cf7b
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-02-05 20:43:21 +0300

    feat: update error handling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import { EOL } from "node:os";
+import { EOL, homedir } from "node:os";
 import { stdin as input, stdout as output, exit } from "node:process";
 import { createInterface } from "node:readline/promises";
 import getUserNameFromArgs from "./utils/getUserNameFromArgs.js";
 import InvalidInputError from "./types/InvalidInputError.js";
+// import readFile from "./fs/readFile.js";
+import { spawn } from "node:child_process";
+import { Worker } from "node:worker_threads";
 
-const run = async () => {
+const run1 = async () => {
   const userName = getUserNameFromArgs();
+
+  const cwd = homedir();
 
   const readlineInterface = createInterface({ input, output });
 
   readlineInterface.write(
     `Welcome to the File Manager, ${userName + "!" + EOL}` +
-      `You are currently in ${"path_to_working_directory" + EOL}`
+      `You are currently in ${cwd + EOL}`
   );
 
   process.on("exit", () => {
@@ -27,14 +32,25 @@ const run = async () => {
 
       if (cmd === ".exit") exit();
 
-      if (cmd === "cat") resolve("OK");
+      if (cmd.startsWith("cat")) {
+        const argsArray = cmd.split(" ");
+        if (argsArray.length !== 2) reject(new InvalidInputError());
+        resolve(readFile());
+      }
+
+      if (cmd === "error") resolve(new Error());
 
       reject(new InvalidInputError());
     })
-      .then((message) => {
-        readlineInterface.write(message + EOL);
+      .then((data) => {
+        if (data instanceof Error) {
+          throw new Error(data);
+        } else {
+          readlineInterface.write(data + EOL);
+        }
       })
       .catch((error) => {
+        console.log("promise catch");
         if (error instanceof InvalidInputError) {
           readlineInterface.write(error.message + EOL);
         } else {
@@ -42,11 +58,153 @@ const run = async () => {
         }
       })
       .finally(() => {
-        readlineInterface.write(
-          `You are currently in ${"path_to_working_directory" + EOL}`
-        );
+        readlineInterface.write(`You are currently in ${cwd + EOL}`);
       });
   }
 };
 
-await run();
+// switch case
+const run2 = async () => {
+  const userName = getUserNameFromArgs();
+
+  const cwd = homedir();
+
+  const readlineInterface = createInterface({ input, output });
+
+  readlineInterface.write(
+    `Welcome to the File Manager, ${userName + "!" + EOL}` +
+      `You are currently in ${cwd + EOL}`
+  );
+
+  process.on("exit", () => {
+    readlineInterface.write(
+      `${EOL}Thank you for using File Manager, ${userName}, goodbye!`
+    );
+    exit();
+  });
+
+  while (true) {
+    const cmd = await readlineInterface.question(">_ ");
+    const operationName = cmd.split(" ")[0];
+
+    switch (operationName) {
+      case ".exit":
+        exit();
+      case "cat":
+        await readFile(cmd, cwd, readlineInterface);
+        break;
+      default:
+        readlineInterface.write("Invalid input" + EOL);
+        break;
+    }
+    readlineInterface.write(`You are currently in ${cwd + EOL}`);
+  }
+};
+
+// child process
+const run = async () => {
+  const userName = getUserNameFromArgs();
+
+  const cwd = homedir();
+
+  const readlineInterface = createInterface({ input, output });
+
+  readlineInterface.write(
+    `Welcome to the File Manager, ${userName + "!" + EOL}` +
+      `You are currently in ${cwd + EOL}`
+  );
+
+  process.on("exit", () => {
+    readlineInterface.write(
+      `${EOL}Thank you for using File Manager, ${userName}, goodbye!`
+    );
+    exit();
+  });
+
+  while (true) {
+    const cmd = await readlineInterface.question(">_ ");
+    const operationName = cmd.split(" ")[0];
+
+    const spawnChildProcess = async (args) => {
+      const command = "node";
+      spawn(command, [...args], {
+        stdio: ["inherit", "inherit", "inherit"],
+      });
+    };
+
+    switch (operationName) {
+      case ".exit":
+        exit();
+      case "cat":
+        await spawnChildProcess(["./src/fs/readFile.js", cmd, cwd]);
+
+        break;
+      default:
+        readlineInterface.write("Invalid input" + EOL);
+        readlineInterface.write(`You are currently in ${cwd + EOL}`);
+        break;
+    }
+  }
+};
+
+const runWT = async () => {
+  const userName = getUserNameFromArgs();
+
+  const cwd = homedir();
+
+  const readlineInterface = createInterface({ input, output });
+
+  readlineInterface.write(
+    `Welcome to the File Manager, ${userName + "!" + EOL}` +
+      `You are currently in ${cwd + EOL}`
+  );
+
+  process.on("exit", () => {
+    readlineInterface.write(
+      `${EOL}Thank you for using File Manager, ${userName}, goodbye!`
+    );
+    exit();
+  });
+
+  while (true) {
+    const cmd = await readlineInterface.question(">_ ");
+    const operationName = cmd.split(" ")[0];
+    let workerPath;
+
+    switch (operationName) {
+      case ".exit":
+        exit();
+      case "wt":
+        if (cmd.split(" ").length !== 2) {
+          readlineInterface.write("Invalid input" + EOL);
+        } else {
+          workerPath = "./src/worker.js";
+        }
+        break;
+      case "cat":
+        if (cmd.split(" ").length !== 2) {
+          readlineInterface.write("Invalid input" + EOL);
+        } else {
+          workerPath = "./src/fs/readFile.js";
+        }
+        break;
+      default:
+        readlineInterface.write("Invalid input" + EOL);
+        break;
+    }
+
+    if (workerPath) {
+      await new Promise((response) => {
+        const worker = new Worker(workerPath, { workerData: { cmd, cwd } });
+        worker.on("message", (data) => response(data));
+        worker.on("error", () => response("Operation failed"));
+      })
+        .then((data) => readlineInterface.write(data + EOL))
+        .catch((err) => readlineInterface.write(err));
+    }
+
+    readlineInterface.write(`You are currently in ${cwd + EOL}`);
+  }
+};
+
+await runWT();
